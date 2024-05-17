0cf6c4043ea2376cc1c30d53f760ed6a17bdea6b
mojyack <mojyack@gmail.com>
2024-05-17 18:03:29 +0900

    example: helper: cleanup

--- src/examples/helper.cpp
+++ src/examples/helper.cpp
@@ -10,7 +10,7 @@ auto add_new_element_to_pipeine(GstElement* const pipeline, const char* const el
     auto elm = gst_element_factory_make(element_name, NULL);
     assert_p(elm != NULL);
     assert_p(gst_bin_add(GST_BIN(pipeline), elm) == TRUE);
-    assert_p(gst_element_sync_state_with_parent(elm));
+    assert_p(gst_element_sync_state_with_parent(elm) == TRUE);
     return elm;
 }
 
